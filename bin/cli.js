a183586e94e8cb448ca950d32681c1c446a9305d
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2022-03-10 13:26:32 +0700

    initial commit

---
+++ bin/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const {execSync} = require('child_process');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch(e) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 git@gitlab.dataon.com:react-library/master-custom.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installeDeps = runCommand(installDepsCommand);
+
+if(!installeDeps) process.exit(-1);
+
+console.log("Congratulations!");
+console.log(`cd ${repoName} && npm start`);
