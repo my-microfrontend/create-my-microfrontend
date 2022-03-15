997b1dc44157b6f21bd3efee2e72f45d7f87fb09
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2022-03-15 17:37:28 +0700

    update

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 
-const {execSync} = require('child_process');
+const { execSync, exec } = require("child_process");
 
-const runCommand = command => {
+const runCommand = (command, type = "linux") => {
     try {
-        execSync(`${command}`, {stdio: 'inherit'});
-    } catch(e) {
+        if (type === "win") {
+            exec(command, (err, stdout, stderr) => {
+                if (err) {
+                    console.error(`Failed to execute ${err}`);
+                    return false;
+                }
+            });
+        } else {
+            execSync(`${command}`, { stdio: "inherit" });
+        }
+    } catch (e) {
         console.error(`Failed to execute ${command}`, e);
         return false;
     }
     return true;
-}
+};
 
 const repoName = process.argv[2] || "micro";
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/ugiispoyo/Micro-Id.git ${repoName}`;
@@ -19,17 +28,20 @@ const gitCheckoutCommand = `git clone --depth 1 https://github.com/ugiispoyo/Mic
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 
-if(!checkedOut) process.exit(-1);
+if (!checkedOut) process.exit(-1);
 
-let removeOther;
+let removeOther, execRemoveOther;
 const opsys = process.platform;
 if (opsys == "darwin" || opsys == "linux") {
     removeOther = `cd ${repoName} && rm -rf bin && rm .npmignore && git remote rm origin`;
+    execRemoveOther = runCommand(removeOther);
+    if (!execRemoveOther) process.exit(-1);
 } else if (opsys == "win32" || opsys == "win64") {
-    removeOther = `cd ${repoName} && rmdir bin && del .npmignore && git remote rm origin`;
-} 
-const execRemoveOther = runCommand(removeOther);
-if(!execRemoveOther) process.exit(-1);
+    removeOther = `cd ${repoName} && rmdir "bin" && del .npmignore && git remote rm origin`;
+    execRemoveOther = runCommand(removeOther, "win");
+}
+
+if (!execRemoveOther) process.exit(-1);
 
 // console.log(`Installing dependencies for ${repoName}`);
 // const installeDeps = runCommand(installDepsCommand);
