b27d5094f961cd5e197cccb3b88579406ed17461
Ugi Ispoyo Widodo <ugi.widodo@dataon.com>
2022-03-15 21:24:21 +0700

    update cli

--- bin/cli.js
+++ bin/cli.js
@@ -20,7 +20,7 @@ if (
 ) {
     appFramework = process.argv[3].slice(2, process.argv[3].length) + "-app";
 }
-console.log(appFramework);
+console.log(`Create ${appFramework} ⬅️`);
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/ugiispoyo/Micro-Id.git ${repoName}`;
 
 console.log(`Cloning the repository with name ${repoName}`);
@@ -32,10 +32,10 @@ let removeOther, execRemoveOther;
 const opsys = process.platform;
 if (opsys == "darwin" || opsys == "linux") {
     // removeOther = `cd ${repoName} && rm -rf bin && rm .npmignore && git remote rm origin`;
-    removeOther = `cd ${repoName} && mv ${appFramework}/* ../${repoName} && rm -rf react-app && rm -rf vue-app && rm -rf bin && rm .npmignore && git remote rm origin`;
+    removeOther = `cd ${repoName} && rm package.json && mv ${appFramework}/* ../${repoName} && rm -rf react-app && rm -rf vue-app && rm -rf bin && rm .npmignore && git remote rm origin`;
     execRemoveOther = runCommand(removeOther);
 } else if (opsys == "win32" || opsys == "win64") {
-    removeOther = `cd ${repoName} && mv ${appFramework}/* ../${repoName} && rmdir /S /q react-app && rmdir /S /q vue-app && rmdir /S /q "bin" && del .npmignore && git remote rm origin`;
+    removeOther = `cd ${repoName} && del package.json && mv ${appFramework}/* ../${repoName} && rmdir /S /q react-app && rmdir /S /q vue-app && rmdir /S /q "bin" && del .npmignore && git remote rm origin`;
     execRemoveOther = runCommand(removeOther);
 }
 
@@ -47,6 +47,8 @@ if (!execRemoveOther) process.exit(-1);
 
 // if(!installeDeps) process.exit(-1);
 
-console.log("🎊 Congratulations! 🎊");
-console.log("Happy coding 😃");
-console.log(`➡️ cd ${repoName} && npm start ⬅️`);
+console.log("🎊 🎊 🎊 Congratulations! 🎊 🎊 🎊");
+console.log("😃 Happy coding 😃");
+console.log("");
+console.log(`cd ${repoName} && npm start`);
+console.log("");
